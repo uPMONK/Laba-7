1fe5b2d13a0b8996caf26879ca63383b695051ec
bozin <bozin@DESKTOP-ACF1RHN>
2022-04-07 21:55:24 +0700

    Добавьте файлы проекта.

---
+++ StudentList/Models/ControlMark.cs
@@ -0,0 +1,73 @@
+﻿using Avalonia.Interactivity;
+using Avalonia.Media;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace StudentList.Models
+{
+    public class ControlMark : INotifyPropertyChanged
+    {
+        float? mark;
+
+        [XmlIgnore]
+        public Avalonia.Media.IBrush Brush { get; private set; }
+
+        public float? Mark
+        {
+            set
+            {
+                switch (value)
+                {
+                    case 0:
+                        this.Brush = Brushes.Red;
+                        this.mark = value;
+                        break;
+                    case 1:
+                        this.Brush = Brushes.Yellow;
+                        this.mark = value;
+                        break;
+                    case 2:
+                        this.Brush = Brushes.LightGreen;
+                        this.mark = value;
+                        break;
+                    default:
+                        this.Brush = Brushes.White;
+                        this.mark = null;
+                        break;
+                }
+                RaisePropertyChangedEvent("Mark");
+
+            }
+            get
+            {
+                return this.mark;
+            }
+        }
+        public ControlMark(float mark)
+        {
+            this.Mark = mark;
+        }
+
+        public ControlMark()
+        {
+            this.Mark = 0;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChangedEvent(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChangedEventArgs e = new PropertyChangedEventArgs(propertyName);
+                PropertyChanged(this, e);
+            }
+        }
+    }
+
+}
---
+++ StudentList/ViewModels/MainViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentList.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
